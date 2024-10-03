675d50fe2758c24b133cedad282432428e9b0875
CrossEyedCat <110236400+CrossEyedCat@users.noreply.github.com>
2024-10-03 16:24:59 +0300

    Доработка логики

--- src/main/java/ru/konstantinpetrov/mailresponse/backend/jwt/JwtUtil.java
+++ src/main/java/ru/konstantinpetrov/mailresponse/backend/jwt/JwtUtil.java
@@ -2,7 +2,6 @@ package ru.konstantinpetrov.mailresponse.backend.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
