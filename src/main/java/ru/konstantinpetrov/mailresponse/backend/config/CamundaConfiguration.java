53950aabee05f331b46cf52905287c978fcd9d33
CrossEyedCat <110236400+CrossEyedCat@users.noreply.github.com>
2024-10-18 23:37:47 +0300

    Исправление настроек

--- src/main/java/ru/konstantinpetrov/mailresponse/backend/config/CamundaConfiguration.java
+++ src/main/java/ru/konstantinpetrov/mailresponse/backend/config/CamundaConfiguration.java
@@ -1,13 +1,12 @@
 package ru.konstantinpetrov.mailresponse.backend.config;
 
-
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.repository.DeploymentBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Configuration
@@ -15,11 +14,18 @@ public class CamundaConfiguration {
 
     private static final String BPMN_PATH = "classpath:/bpmn/*.bpmn";
 
+    private final RepositoryService repositoryService;
+
+    // Constructor injection of RepositoryService
+    public CamundaConfiguration(RepositoryService repositoryService) {
+        this.repositoryService = repositoryService;
+    }
+
     /**
-     * Автоматический деплой всех BPMN процессов при запуске приложения.
+     * Automatically deploy all BPMN processes at application startup.
      */
-    @Bean
-    public void deployProcesses(RepositoryService repositoryService) throws IOException {
+    @PostConstruct
+    public void deployProcesses() throws IOException {
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resolver.getResources(BPMN_PATH);
 
